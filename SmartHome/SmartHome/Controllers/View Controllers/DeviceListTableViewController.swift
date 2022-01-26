4d0873c51ba344dcc1aaae36dcfcc6929dfa4fed
Arian Mohajer <amohajer45@gmail.com>
2022-01-26 13:34:13 -0700

    implemented delegate/protocol, switch and corresponding toggle actions. Redy for submission

--- SmartHome/SmartHome/Controllers/View Controllers/DeviceListTableViewController.swift
+++ SmartHome/SmartHome/Controllers/View Controllers/DeviceListTableViewController.swift
@@ -16,7 +16,6 @@ class DeviceListTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return DeviceController.sharedInstance.devices.count
     }
 
@@ -24,11 +23,49 @@ class DeviceListTableViewController: UITableViewController {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "deviceCell", for: indexPath) as? DeviceTableViewCell else {return UITableViewCell()}
 
         // Configure the cell...
+        let device = DeviceController.sharedInstance.devices[indexPath.row]
+        cell.updateCell(device: device)
+        cell.delegate = self
 
         return cell
     }
     
-    @IBAction func addDeviceButtonTapped(_ sender: Any) {
+    func presentAlertController() {
+        //Create alert controller
+        let alertController = UIAlertController(title: "New Smart Device", message: "Enter the device's name", preferredStyle: .alert)
+        //Create actions
+        alertController.addTextField { textField in
+            textField.placeholder = "Smart Device Name..."
+        }
+        
+        //Add actions to alert controller
+        let dismissAction = UIAlertAction(title: "Cancel", style: .destructive)
+        alertController.addAction(dismissAction)
+        
+        let confirmAction = UIAlertAction(title: "Save", style: .default) { _ in
+            guard let deviceNameText = alertController.textFields?.first?.text else { return }
+            DeviceController.sharedInstance.createDevice(named: deviceNameText)
+            self.tableView.reloadData()
+        }
+        alertController.addAction(confirmAction)
+
+        
+        //Present the alert controller
+        present(alertController, animated: true)
     }
     
+    @IBAction func addDeviceButtonTapped(_ sender: Any) {
+        presentAlertController()
+    }
+    
+}
+
+extension DeviceListTableViewController: DeviceTableViewCellDelegate {
+    func toggleIsOn(cell: DeviceTableViewCell) {
+        guard let indexPath = tableView.indexPath(for: cell) else { return }
+        let device = DeviceController.sharedInstance.devices[indexPath.row]
+        DeviceController.sharedInstance.toggleIsOn(device: device)
+        cell.updateCell(device: device)
+        print(DeviceController.sharedInstance.devices[indexPath.row].isOn)
+    }
 }
--- SmartHome/SmartHome/Controllers/View Controllers/DeviceTableViewCell.swift
+++ SmartHome/SmartHome/Controllers/View Controllers/DeviceTableViewCell.swift
@@ -7,12 +7,25 @@
 
 import UIKit
 
+protocol DeviceTableViewCellDelegate: AnyObject {
+    func toggleIsOn(cell: DeviceTableViewCell)
+}
+
 class DeviceTableViewCell: UITableViewCell {
     // MARK: - IBOutlets
     @IBOutlet weak var deviceNameLabel: UILabel!
     @IBOutlet weak var deviceSwitch: UISwitch!
     
+    weak var delegate: DeviceTableViewCellDelegate?
     
+    // MARK: - Helper Function
+    func updateCell(device: Device) {
+        deviceNameLabel.text = device.name
+        deviceSwitch.isOn = device.isOn
+    }
+    
+    // MARK: - IBActions
     @IBAction func deviceSwitchToggled(_ sender: Any) {
+        delegate?.toggleIsOn(cell: self)
     }
 }
